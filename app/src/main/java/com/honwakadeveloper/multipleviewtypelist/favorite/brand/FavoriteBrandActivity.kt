22d0fdc6fa848ff9c93954a108703bbf3bd03ab7
Honwaka Developer <honwaka.developer@gmail.com>
2019-05-28 17:48:34 +0900

    first commit

---
+++ app/src/main/java/com/honwakadeveloper/multipleviewtypelist/favorite/brand/FavoriteBrandActivity.kt
@@ -0,0 +1,13 @@
+package com.honwakadeveloper.multipleviewtypelist.favorite.brand
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.honwakadeveloper.multipleviewtypelist.R
+
+class FavoriteBrandActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite_brand)
+    }
+}
